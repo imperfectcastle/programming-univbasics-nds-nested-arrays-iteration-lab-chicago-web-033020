1169d77d2e6a3e6740d383d8428363d035a9593b
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-14 22:38:44 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,6 +49,7 @@ def total_even_pairs(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
+      binding.pry 
       if src[row_index][element_index] % 2 == 0
         #element_index += 1
         #src[row_index][element_index]
