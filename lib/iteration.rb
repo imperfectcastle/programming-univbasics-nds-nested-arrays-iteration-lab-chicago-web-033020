729ef83716883f00e842ab643d496689abe550a7
Bart Sakowski <bart.sakowski@gmail.com>
2020-03-14 19:23:34 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -41,6 +41,10 @@ def find_greater_pair(src)
 
 
 def total_even_pairs(src)
+
+
+
+
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
